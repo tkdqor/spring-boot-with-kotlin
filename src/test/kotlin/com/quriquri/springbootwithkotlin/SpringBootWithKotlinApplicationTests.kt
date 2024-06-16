9bc5e891f5018efa3ab22ddc0d8504113383477f
tkdqor <aumsbk@naver.com>
2024-06-17 00:54:18 +0900

    init: spring-boot-with-kotlin 로컬 프로젝트를 github 레포에 올리기

---
+++ src/test/kotlin/com/quriquri/springbootwithkotlin/SpringBootWithKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.quriquri.springbootwithkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootWithKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
