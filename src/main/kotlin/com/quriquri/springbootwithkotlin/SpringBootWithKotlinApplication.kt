2709d8d379eb0d505c989fa490776628b2a6861d
tkdqor <aumsbk@naver.com>
2024-06-20 03:29:55 +0900

    feature: db 세팅 전까지 SpringBootWithKotlinApplication에 DataSourceAutoConfiguration를 exclude로 설정

--- src/main/kotlin/com/quriquri/springbootwithkotlin/SpringBootWithKotlinApplication.kt
+++ src/main/kotlin/com/quriquri/springbootwithkotlin/SpringBootWithKotlinApplication.kt
@@ -1,9 +1,10 @@
 package com.quriquri.springbootwithkotlin
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [DataSourceAutoConfiguration::class])
 class SpringBootWithKotlinApplication
 
 fun main(args: Array<String>) {
