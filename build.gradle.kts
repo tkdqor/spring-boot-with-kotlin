c4eb7508e7644dd2da8b67809af43ee1fe308364
tkdqor <aumsbk@naver.com>
2024-06-23 21:22:41 +0900

    feature: http-app과 product-domain 삭제

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,8 +21,6 @@ repositories {
 
 dependencies {
     // 모듈 의존성
-    implementation(project(":http-app"))
-    implementation(project(":product-domain"))
 
     // Spring boot 라이브러리
     implementation("org.springframework.boot:spring-boot-starter-web")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,1 @@
 rootProject.name = "spring-boot-with-kotlin"
-include("http-app")
-include("product-domain")
-
